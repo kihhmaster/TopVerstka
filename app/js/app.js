69cb0c29b3a46f3d37bad346b5b64ee5b683afa4
kihhmaster <kihhmaster@yandex.ru>
2022-04-03 19:50:13 +0300

    Finish

---
+++ app/js/app.js
@@ -0,0 +1,40 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+import { Swiper, Parallax, Mousewheel, Pagination, Scrollbar, Navigation, Controller} from 'swiper'
+Swiper.use([ Parallax, Mousewheel, Pagination, Scrollbar, Navigation, Controller ])
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+	const sliderTop = new Swiper('.slider__top', {
+		speed: 800,
+		// autoHeight: true,
+		loop: false,
+		slidesPerView: 1,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+		},
+
+
+	})
+
+	let sliderStartPanel = document.querySelector('.slider__startPanel');
+	let buttonStartPanel = document.querySelector('.button__startPanel');
+	let sliderTopStart = document.querySelector('.slider__top');
+
+	function buttonStart() {
+		sliderStartPanel.style.display = "none";
+		sliderTopStart.style.display = "flex";
+		sliderTopStart.classList.add("animate__fadeInRight")   
+	}
+	buttonStartPanel.addEventListener("click", ()=>{
+		setTimeout(buttonStart, 1000);
+	})
+
+
+
+})
